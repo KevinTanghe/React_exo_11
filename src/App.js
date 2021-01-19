6f9db5dfc3e688767f5a9a2467f386b5372200c4
Tanghe Kevin <tanghekevin1@gmail.com>
2021-01-19 11:03:29 +0100

    projet finis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    article : [
+      {name: "Ventilateur: ", prix: "3€"},
+      {name: "Manteau: ", prix: "10€"},
+    ],
+  };
+  render() {
+
+    
+
+    if (this.state.article.length <= 2) {
+      return (
+        <div>
+          <h1>Liste du magasin</h1>
+          <div>
+            <span>{this.state.article[0].name}</span>
+            <span>{this.state.article[0].prix}</span>
+          </div>
+          <div>
+            <span>{this.state.article[1].name}</span>
+            <span>{this.state.article[1].prix}</span>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>Il y a plus rien</h1>
+        </div>
+      )
+    }
+  }
 }
 
-export default App;
+export default App
+
